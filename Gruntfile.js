4e1a40fd0ff91920b24739f19753a4b3c946258d
superRaytin <superRaytin@163.com>
2014-08-29 12:42:25 +0800

    Add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+﻿module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*\n' +
+                    ' * xssFilter <%= pkg.version %>\n' +
+                    ' * <%= pkg.description %>\n' +
+                    ' * https://github.com/superRaytin/xssFilter\n' +
+                    ' *\n' +
+                    ' * Copyright 2014, SuperRaytin\n' +
+                    ' * Released under the MIT license.\n' +
+                    '*/\n\n'
+            },
+            main: {
+                files: [
+                    {
+                        src: ['dist/xssFilter-debug.js'],
+                        dest: 'dist/xssFilter.js'
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('build', ['uglify']);
+};
