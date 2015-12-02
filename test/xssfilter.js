411b9246d5c84bf429655260a9d09e408eb9f72d
leon.shi <superRaytin@163.com>
2015-12-03 00:13:29 +0800

    Add test case

--- test/xssfilter.js
+++ test/xssfilter.js
@@ -2,7 +2,7 @@
 var should = require('should');
 var xssFilter = require('../lib/index');
 
-var htmlHead = '<div class="like" ondblclick= "ondblclick(); return false;" onmousedown="mousedown()">';
+var htmlHead = '<div class="like"  ondblclick= "ondblclick(); return false;"  onmousedown="mousedown()">';
 var text = 'something...';
 var htmlFoot = '</div>';
 var defaultScript = '<script>alert(88)</script>';
@@ -19,10 +19,14 @@ var div2 = '<div class="desc" onsubmit="load()">desc</div>';
 
 var defaultHTML = htmlHead + text + htmlFoot;
 
-describe('XSS Filter', function() {
+describe('filter', function() {
   it('should filter() work', function() {
     var xssfilter = new xssFilter();
     xssfilter.filter(defaultHTML).should.equal('<div class="like">something...</div>');
+    xssfilter.options('blackListAttrs', {
+      onmousedown: false
+    });
+    xssfilter.filter('<div class="like"     ondblclick= "ondblclick(); return false;"       onmousedown  ="mousedown()"    >something...</div>').should.equal('<div class="like" onmousedown="mousedown()">something...</div>');
   });
 
   it('should options() work', function() {
@@ -49,75 +53,84 @@ describe('XSS Filter', function() {
 
   it('should blackListAttrs work', function() {
     var xssfilter = new xssFilter();
-    xssfilter.options('blackList_attrs', {
+    xssfilter.options('blackListAttrs', {
       ondblclick: false
     });
-    xssfilter.filter(defaultHTML).should.equal('<div class="like" ondblclick= "ondblclick(); return false;">something...</div>');
+    xssfilter.filter(defaultHTML).should.equal('<div class="like" ondblclick="ondblclick(); return false;">something...</div>');
 
-    xssfilter.options('blackList_attrs', {
+    xssfilter.options('blackListAttrs', {
       onmousedown: false
     });
-    xssfilter.filter(defaultHTML).should.equal('<div class="like" ondblclick= "ondblclick(); return false;" onmousedown="mousedown()">something...</div>');
+    xssfilter.filter(defaultHTML).should.equal('<div class="like" ondblclick="ondblclick(); return false;" onmousedown="mousedown()">something...</div>');
 
-    xssfilter.options('blackList_attrs', {
+    xssfilter.options('blackListAttrs', {
       ondblclick: true,
       onmousedown: false
     });
-    xssfilter.filter(defaultHTML).should.equal('<div class="like"  onmousedown="mousedown()">something...</div>');
+    xssfilter.filter(defaultHTML).should.equal('<div class="like" onmousedown="mousedown()">something...</div>');
   });
 
-  it('should beautifyTags work', function() {
+  it('should cleanTag work', function() {
     var xssfilter = new xssFilter();
-    xssfilter.options('beautifyTags', false);
-    xssfilter.filter(defaultHTML).should.equal('<div class="like"  >something...</div>');
+    xssfilter.options('cleanTag', false);
+    xssfilter.filter(defaultHTML).should.equal('<div class="like"    >something...</div>');
 
-    xssfilter.options('beautifyTags', true);
+    xssfilter.options('cleanTag', true);
     xssfilter.filter(defaultHTML).should.equal('<div class="like">something...</div>');
+    xssfilter.filter('<div    class="like"     ondblclick= "ondblclick();      return false;"     onmousedown    =           "mousedown();     alert(8888)" aaa bbb   >something...</div>').should.equal('<div class="like" aaa bbb>something...</div>');
   });
 
-  it('should label_style work', function() {
+  it('should matchStyleTag work', function() {
     var xssfilter = new xssFilter();
 
-    xssfilter.options('label_style', false);
+    xssfilter.options('matchStyleTag', false);
     var html = htmlHead + text + defaultStyle + htmlFoot;
     xssfilter.filter(html).should.equal('<div class="like">something...'+ defaultStyle +'</div>');
 
-    xssfilter.options('label_style', false);
+    xssfilter.options('matchStyleTag', false);
     html = htmlHead + text + defaultMultiLineStyle + htmlFoot;
     xssfilter.filter(html).should.equal('<div class="like">something...'+ defaultMultiLineStyle +'</div>');
 
-    xssfilter.options('label_style', true);
+    xssfilter.options('matchStyleTag', true);
     html = htmlHead + text + defaultStyle + htmlFoot;
     xssfilter.filter(html).should.equal('<div class="like">something...</div>');
 
-    xssfilter.options('label_style', true);
+    xssfilter.options('matchStyleTag', true);
     html = htmlHead + text + defaultMultiLineStyle + htmlFoot;
     xssfilter.filter(html).should.equal('<div class="like">something...</div>');
   });
 
-  it('should label_script work', function() {
+  it('should matchScriptTag work', function() {
     var xssfilter = new xssFilter();
 
-    xssfilter.options('label_script', false);
+    xssfilter.options('matchScriptTag', false);
     var html = htmlHead + text + defaultScript + htmlFoot;
     xssfilter.filter(html).should.equal('<div class="like">something...'+ defaultScript +'</div>');
 
-    xssfilter.options('label_script', false);
+    xssfilter.options('matchScriptTag', false);
     html = htmlHead + text + defaultMultiLineScript + htmlFoot;
     xssfilter.filter(html).should.equal('<div class="like">something...'+ defaultMultiLineScript +'</div>');
 
-    xssfilter.options('label_script', true);
+    xssfilter.options('matchScriptTag', true);
     html = htmlHead + text + defaultScript + htmlFoot;
     xssfilter.filter(html).should.equal('<div class="like">something...</div>');
 
-    xssfilter.options('label_script', true);
+    xssfilter.options('matchScriptTag', true);
     html = htmlHead + text + defaultMultiLineScript + htmlFoot;
     xssfilter.filter(html).should.equal('<div class="like">something...</div>');
+  });
+
+  it('should work when attribute value was double quoted or single quoted or unquoted', function() {
+    var xssfilter = new xssFilter();
+    xssfilter.filter('<div class="like" onmousedown="mousedown()">something...</div>').should.equal('<div class="like">something...</div>');
+    xssfilter.filter("<div class='like' onmousedown='mousedown()'>something...</div>").should.equal("<div class='like'>something...</div>");
+    xssfilter.filter('<div class=likebbbbbbbbbbbbbb onmousedown=mousedown()>something...</div>').should.equal('<div class=likebbbbbbbbbbbbbb>something...</div>');
   });
 
   it('should work when there are spaces between attribute name and "=", like class ="like"', function() {
     var xssfilter = new xssFilter();
-    xssfilter.filter('<div class ="like" ondblclick= "ondblclick(); return false;" onmousedown="mousedown()">something...</div>').should.equal('<div class ="like">something...</div>');
-    xssfilter.filter('<div class = "like" ondblclick= "ondblclick(); return false;" onmousedown="mousedown()">something...</div>').should.equal('<div class = "like">something...</div>');
+    xssfilter.filter('<div class ="like" ondblclick= "ondblclick(); return false;" onmousedown="mousedown()">something...</div>').should.equal('<div class="like">something...</div>');
+    xssfilter.filter('<div class = "like" ondblclick= "ondblclick(); return false;" onmousedown="mousedown()">something...</div>').should.equal('<div class="like">something...</div>');
+    xssfilter.filter('<div class =  "like" ondblclick= "ondblclick();    return false;" onmousedown="mousedown()">something...</div>').should.equal('<div class="like">something...</div>');
   });
 });
